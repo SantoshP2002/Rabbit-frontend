0c1790e22da44c994c2eb9b0aba7aabf77e868f3
SantoshP2002 <santoshpawar96k@gmail.com>
2026-04-06 14:28:20 +0530

    create Constants file

---
+++ src/constants/index.jsx
@@ -0,0 +1,43 @@
+export const banners = [
+  {
+    id: 1,
+    title: "Summer Sale",
+    subtitle: "Up to 50% Off",
+    image:
+      "https://img.freepik.com/free-vector/summer-colection-banner-with-hand-drawn-flowers_1188-312.jpg",
+  },
+  {
+    id: 2,
+    title: "New Arrivals",
+    subtitle: "Latest Trends",
+    image:
+      "https://strapi-wasabi-bucket-prod-cdn.phot.ai/large_Showcase_New_Arrivals_with_AI_Fashion_Banner_Maker_047bd356df.webp",
+  },
+  {
+    id: 3,
+    title: "Electronics",
+    subtitle: "Smart Deals",
+    image:
+      "https://img.freepik.com/free-psd/neon-banner-template-clothing-store-sale_23-2149001696.jpg?semt=ais_rp_progressive&w=740&q=80",
+  },
+  {
+    id: 4,
+    title: "Shoes",
+    subtitle: "Comfort & Style",
+    image:
+      "https://img.freepik.com/free-vector/modern-black-friday-sale-banner-template-with-3d-background-red-splash_1361-1877.jpg?semt=ais_incoming&w=740&q=80",
+  },
+  {
+    id: 5,
+    title: "Watches",
+    subtitle: "Luxury Picks",
+    image: "https://sylvi.in/cdn/shop/articles/85_1.jpg?v=1676549659",
+  },
+  {
+    id: 6,
+    title: "Accessories",
+    subtitle: "Complete Your Look",
+    image:
+      "https://cdn.dribbble.com/userupload/9839613/file/original-d2dfdc4f45b4f795f705834dfc70801d.jpg?resize=752x&vertical=center",
+  },
+];
