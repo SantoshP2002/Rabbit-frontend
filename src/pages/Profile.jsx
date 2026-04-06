51b86d8cf1c4940a8354743d36ea8eef8da18b37
SantoshP2002 <santoshpawar96k@gmail.com>
2026-04-06 16:52:22 +0530

    profile page in css changes

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,6 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
+// eslint-disable-next-line no-unused-vars
+import { motion } from "framer-motion";
 import MyOrdersPage from "./MyOrdersPage";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
@@ -29,26 +31,39 @@ const Profile = () => {
       <div className="flex-grow container mx-auto p-4 md:p-6">
         <div className="flex flex-col md:flex-row md:space-x-6 space-y-6 md:space-y-0">
           {/* LEFT SECTION */}
-          <div className="w-full md:w-1/3 lg:w-1/4 bg-white shadow-xl rounded-2xl p-6 transition duration-300 hover:shadow-2xl">
+          <motion.div
+            initial={{ opacity: 0, y: 30 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.4 }}
+            whileHover={{ y: -5 }}
+            className="w-full md:w-1/3 lg:w-1/4 bg-white/80 backdrop-blur-lg shadow-lg rounded-2xl p-6 border hover:shadow-2xl transition-all duration-300"
+          >
             {/* USER NAME */}
-            <h1 className="text-2xl font-extrabold text-gray-800 mb-2 tracking-wide">
+            <h1 className="text-xl sm:text-2xl font-bold text-gray-800 mb-1 tracking-wide">
               {user?.name}
             </h1>
 
             {/* EMAIL */}
-            <p className="text-base text-gray-500 mb-6 uppercase">{user?.email}</p>
+            <p className="text-sm text-gray-500 mb-5 break-all">
+              {user?.email}
+            </p>
 
             {/* LOGOUT BUTTON */}
-            <button
+            <motion.button
+              whileTap={{ scale: 0.95 }}
+              whileHover={{ scale: 1.02 }}
               onClick={handelLogout}
-              className="relative overflow-hidden w-full border border-gray-800 text-gray-800 font-semibold py-2 px-4 rounded-lg group"
+              className="relative overflow-hidden w-full border border-gray-800 text-gray-800 font-medium py-2.5 px-4 rounded-xl group"
             >
-              <span className="absolute inset-0 bg-gray-900 transition-all duration-500 ease-in-out transform -translate-x-full group-hover:translate-x-0 z-0 rounded-lg"></span>
+              {/* ANIMATION BG */}
+              <span className="absolute inset-0 bg-gray-900 transition-all duration-500 ease-in-out transform -translate-x-full group-hover:translate-x-0 z-0 rounded-xl"></span>
+
+              {/* TEXT */}
               <span className="relative z-10 group-hover:text-white transition duration-300">
                 Logout
               </span>
-            </button>
-          </div>
+            </motion.button>
+          </motion.div>
 
           {/* RIGHT SECTION: Orders Table */}
           <div className="w-full md:w-2/3 lg:w-3/4">
