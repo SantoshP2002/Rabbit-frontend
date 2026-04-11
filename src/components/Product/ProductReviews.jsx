8a20f0eb612fa83889244238e1128c8b2960859a
SantoshP2002 <santoshpawar96k@gmail.com>
2026-04-11 19:57:02 +0530

    Error Toast msg Update in ProductReview page component

--- src/components/Product/ProductReviews.jsx
+++ src/components/Product/ProductReviews.jsx
@@ -39,7 +39,9 @@ const ProductReviews = () => {
     }
     if (error) {
       toast.error(
-        typeof error === "string" ? error : "Failed to create review",
+        typeof error === "string"
+          ? error
+          : "You already reviewed this product.",
       );
     }
   }, [success, error]);
@@ -309,7 +311,7 @@ const ProductReviews = () => {
           </div>
 
           {/* Buttons */}
-          <div className="flex justify-center items-center gap-10">
+          <div className="flex justify-center items-center gap-2">
             <button
               onClick={() => setShowForm(false)}
               className="mt-6 bg-white text-black border-2 border-black text-sm py-2 px-5 rounded-lg shadow-[5px_5px_0_0_#000] transition-all hover:translate-x-1 hover:translate-y-1 hover:shadow-none"
