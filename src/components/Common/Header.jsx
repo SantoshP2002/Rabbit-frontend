743202d2d8c5cfd87a4f19fc5c6eae02dc21d050
SantoshP2002 <santoshpawar96k@gmail.com>
2026-04-14 18:55:22 +0530

    Headers component in remove comment line

--- src/components/Common/Header.jsx
+++ src/components/Common/Header.jsx
@@ -1,17 +1,14 @@
-import React from 'react'
-import Topbar from '../Layout/Topbar'
-import Navbar from './Navbar'
+import React from "react";
+import Topbar from "../Layout/Topbar";
+import Navbar from "./Navbar";
 
 const Header = () => {
   return (
-      <header className='border-b border-gray-200'>
-          {/* TopBar */}
-          <Topbar/>
-          {/* Navbar */}
-          <Navbar/>
-          {/* Cart drawer */}
+    <header className="border-b border-gray-200">
+      <Topbar />
+      <Navbar />
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
